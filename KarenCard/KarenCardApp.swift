2762f66bd73637fd6fb3f6e861e6bdbbde4d6e04
KarVarr <megatr9n@gmail.com>
2022-11-21 16:55:46 +0300

    Initial Commit

---
+++ KarenCard/KarenCardApp.swift
@@ -0,0 +1,17 @@
+//
+//  KarenCardApp.swift
+//  KarenCard
+//
+//  Created by Karen Vardanian on 21.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct KarenCardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
