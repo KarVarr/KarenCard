ad42469f1fa73e930ff1ab3769f78547fc0ca5d3
KarVarr <megatr9n@gmail.com>
2022-11-21 17:38:14 +0300

    added an image and shape Circle

--- KarenCard/ContentView.swift
+++ KarenCard/ContentView.swift
@@ -11,10 +11,21 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             Color(red: 0.56, green: 0.74, blue: 0.56).edgesIgnoringSafeArea(.all)
-            Text("Karen Vardanian!")
-                .font(Font.custom("Pacifico-Regular", size: 40))
-                .bold()
-                .foregroundColor(.white)
+            VStack {
+                Image("Karen")
+                    .resizable()
+                    .aspectRatio( contentMode: .fill)
+                    .frame(width: 150, height: 150)
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 4))
+                Text("Karen Vardanian!")
+                    .font(Font.custom("Pacifico-Regular", size: 40))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("iOS Developer")
+                    .foregroundColor(.white)
+                    .font(.system(size: 25))
+            }
         }
     }
 }
